e6e6f3e47a220b318be1a4fcd18612dceaa683c5
Dennis Mayr <dmayr.dev@gmail.com>
2021-07-29 18:33:29 -0400

    Added `/cats` endpoint to enter cat name and type

--- main.go
+++ main.go
@@ -11,10 +11,22 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, folks")
 }
 
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	return c.String(http.StatusOK, fmt.Sprintf("Your cat name is: %s \nand his type is: %s\n", catName, catType))
+}
+
+//Main program
 func main() {
 	fmt.Println("Welcome to this humble server")
 	e := echo.New()
-	e.GET("/", hello)
 
+	// Routes
+	e.GET("/", hello)
+	e.GET("/cats", getCats)
+
+	//Server start
 	e.Start(":8000")
 }
